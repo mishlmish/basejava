f162a84fa53fa83d0ea396402c778897d8ee2595
Чимаев Максим <chimax.prog@gmail.com>
2022-04-10 16:35:05 +0300

    add a link to the new design

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        if (storage[0].uuid.equals(uuid)) {
+            return storage[0];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                if (i != (size() - 1)) {
+                    System.arraycopy(storage, i + 1, storage, i, size() - 1 - i);
+                }
+                storage[size() - 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        if (storage[0] != null) {
+            for (Resume resume : storage) {
+                if (resume != null) {
+                    size++;
+                }
+            }
+        }
+        return size;
     }
 }
